b35a887154c77bb8d36c259151709f852a2975f2
yangchen1996 <978784945@qq.com>
2020-03-17 11:12:21 +0800

    增加获取openid，修改首页路由

--- src/views/financeclassroom/FinanceClassRoom.vue
+++ src/views/financeclassroom/FinanceClassRoom.vue
@@ -73,6 +73,9 @@
       console.error("1created"+location.href)
       console.error("2created"+this.getUrlKey("code"))
       let urlTemp = process.env.API_ROOT
+      console.error("1*"+urlTemp.indexOf("localhost") == -1)
+      console.error("2*"+this.$store.state.userInfo.openid == null||this.$store.state.userInfo.openid == '')
+      console.error("3*"+this.$store.state.userInfo.openid)
       if(urlTemp.indexOf("localhost") == -1&&(this.$store.state.userInfo.openid == null||this.$store.state.userInfo.openid == '')){
         this.code = this.getUrlKey('code');
         console.error("3created")
